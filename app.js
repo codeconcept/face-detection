a5605e57d95e202d43bad88c54fd933a6d6197a3
codeconcept <contact@codeconcept.fr>
2020-03-09 12:33:45 +0100

    Display uploaded image

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// @ts-nocheck
 console.log("hello world");
 console.log("faceapi.nets", faceapi.nets);
 const fileUpload = document.getElementById("fileUpload");
 fileUpload.addEventListener("change", getImage, false);
+const uploadedImageDiv = document.getElementById("uploadedImage");
 
 const MODEL_URL = "./models";
 
@@ -13,4 +15,18 @@ faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL).then(() => {
 
 function getImage() {
   console.log("images", this.files[0]);
+  const imageToProcess = this.files[0];
+
+  // display uploaded image
+  let newImg = new Image(imageToProcess.width, imageToProcess.height);
+  newImg.src = imageToProcess;
+  newImg.src = URL.createObjectURL(imageToProcess);
+  uploadedImageDiv.style.border = "4px solid #FCB514";
+  uploadedImageDiv.appendChild(newImg);
+
+  processImage(imageToProcess);
+}
+
+function processImage(image) {
+  // TODO
 }
